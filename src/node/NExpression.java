999e666b5a5a8d1d932996020f84808cbb6f3773
zippermonkey <1960727314@qq.com>
2021-07-20 13:28:21 +0800

    修改NExpression的eval_cond_runntime

--- src/node/NExpression.java
+++ src/node/NExpression.java
@@ -20,10 +20,10 @@ public class NExpression extends Node {
         throw new Exception("can not eval this value at run time.");
     }
   
-    public CondResult eval_cond_runtime(ContextIR ctx,List<IR> ir) throws Exception{
-        CondResult ret;
+    public CondResult eval_cond_runntime(ContextIR ctx, List<IR> ir) throws Exception{
+        CondResult ret = new CondResult();
         OpName dest=new OpName("%"+ctx.get_id());
-        ir.add(new IR(IR.OpCode.CMP,dest,this.eval_runtime(ctx,ir)));
+        ir.add(new IR(IR.OpCode.CMP,dest,this.eval_runtime(ctx,ir),new OpName(0),""));
         ret.then_op=IR.OpCode.JNE;
         ret.else_op=IR.OpCode.JEQ;
         return ret;
