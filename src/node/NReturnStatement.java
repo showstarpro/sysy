8c6dfec06921ef7614c1c487aa4b6b6d5c300497
zippermonkey <1960727314@qq.com>
2021-07-22 17:35:46 +0800

    拼写错误

--- src/node/NReturnStatement.java
+++ src/node/NReturnStatement.java
@@ -19,7 +19,7 @@ public class NReturnStatement extends NStatement {
 
     public void print(int indentation, boolean end, PrintStream out) {
         this.printIndentation(indentation, end, out);
-        out.println("Rrturn");
+        out.println("Return");
         if (value != null) {
             value.print(indentation + 1, true, out);
         }
