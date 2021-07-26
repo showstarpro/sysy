d1fa0f230eaf4def68ef303707ce7f37d9b0c6a4
zippermonkey <1960727314@qq.com>
2021-07-26 16:47:07 +0800

    Test

--- src/Compiler.java
+++ src/Compiler.java
@@ -10,17 +10,30 @@ public class Compiler {
 
     static public void main(String argv[]) {
         try {
-            parser p = new parser(new Lexer(new FileReader(argv[0])));
+            String from = argv[0];
+            String out;
+            PrintStream file=null;
+            if (argv.length > 1) {
+                out = argv[3];
+                file = new PrintStream(
+                        new FileOutputStream(out, false));
+            }
+
+            parser p = new parser(new Lexer(new FileReader(from)));
             Object result = p.parse().value;
             NRoot root = p.root;
 //            root.print(0,false,System.out);
             List<IR> ir = new ArrayList<>();
             ContextIR ctx = new ContextIR();
             root.generate_ir(ctx, ir);
-            for(IR i:ir){
+            for (IR i : ir) {
                 i.print(System.out, false);
             }
-            Asm.generate_asm(ir, System.out);
+
+            if (argv.length > 1)
+                Asm.generate_asm(ir, file);
+            else
+                Asm.generate_asm(ir, System.out);
 
         } catch (Exception e) {
             e.printStackTrace();
